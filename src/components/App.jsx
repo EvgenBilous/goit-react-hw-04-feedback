748e2ead9ae94929cb93e0c219377e44017b614e
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-28 12:57:15 -0600

    buttons

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,9 @@ import Statistics from './Statistics/Statistics';
 import { useState } from 'react';
 
 const App = () => {
-  const [good, setGood] = useState(1);
-  const [neutral, setNeutral] = useState(1);
-  const [bad, setBad] = useState(1);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   function reset() {
     setGood(0);
